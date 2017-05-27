4b576fd317321d408847016aac3c4279832847cb
Cassey <cassey@mewe.org>
2017-05-27 22:58:36 +0800

    Added level prototype

--- assets/levels/1.js
+++ assets/levels/1.js
@@ -1,4 +1,4 @@
 LEVELS["1"] = makeNewLevel();
 
-LEVELS["1"][13] = 'G'
-LEVELS["1"][10] = 'B'
+LEVELS["1"][5][5] = 'B'
+LEVELS["1"][8][2] = 'B'
--- assets/main.js
+++ assets/main.js
@@ -4,24 +4,37 @@ var state = { preload: preload, create: create, update: update }
 var game = new Phaser.Game(256, 256, Phaser.AUTO, '', state);
 
 class Entity {
-  constructor(sprite){
+  constructor(spriteName,x,y){
+    const sprite =  game.add.sprite(x, y, spriteName)
+    sprite.anchor.x = 0.5
+    sprite.anchor.y = 0.5
+    
     this.sprite = sprite
     game.physics.enable(sprite, Phaser.Physics.ARCADE);
   }
 }
 
+class Obstacle extends Entity{
+  constructor(spriteName,x,y){
+    super(spriteName,x,y)
+  }
+}
+
+class BrickWall extends Obstacle{
+  constructor(x,y){
+    super("brickWall",x,y)
+  }
+}
+
 class Tank extends Entity {
-  constructor(){
-    const sprite =  game.add.sprite(8, 8, 'player')
-    sprite.anchor.x = 0.5
-    sprite.anchor.y = 0.5
-    super(sprite)
+  constructor(x,y){
+    super("player",x,y)
   } 
 }
 
 class PlayerEntity extends Tank {
   constructor(){
-    super()
+    super(8,8)
     this.keys = {
       p1 : {
         'up': game.input.keyboard.addKey(Phaser.Keyboard.W),
@@ -55,17 +68,26 @@ class PlayerEntity extends Tank {
 
 function preload() {
   game.load.image('player', 'assets/images/player/BigTank1.png');
+  game.load.image('brickWall', 'assets/images/Elements/Bricks.png');
 }
 
 function create() {
 
-  
-  // sprite.body.collideWorldBounds = true;
-  // sprite.body.checkCollision.up = false;
-  // sprite.body.checkCollision.down = false;
-  // sprite.body.immovable = true;
+  this.p1 = new PlayerEntity();
 
-  this.p1 = new PlayerEntity()
+  const currLvl = LEVELS["1"]
+  for (var x = currLvl.length - 1; x >= 0; x--) {
+    for (var y = currLvl[x].length - 1; y >= 0; y--) {
+      const slot = currLvl[x][y]
+      if(!slot) continue;
+
+      switch(slot){
+        case 'B':
+          test = new BrickWall(x * 16,y * 16);
+          break;
+      }
+    }
+  }
 
 
 }
